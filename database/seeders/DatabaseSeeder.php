4971b1471af8207c6294177bac65fb7437c2b3a5
igorakimy <igorakimy@gmail.com>
2025-07-03 11:34:10 +0300

    add: quest type model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\NpcGroups;
+use App\Models\QuestChain;
+use App\Models\QuestType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +22,10 @@ class DatabaseSeeder extends Seeder
 
         $this->call(CharacterClassSeeder::class);
         $this->call(LocationTypeSeeder::class);
+        $this->call(ElementSeeder::class);
+        $this->call(RaceSeeder::class);
+        $this->call(NpcGroups::class);
+        $this->call(QuestChain::class);
+        $this->call(QuestType::class);
     }
 }
---
+++ database/seeders/QuestTypeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\QuestType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class QuestTypeSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $questTypes = [
+            'Обычное',
+            'Ежедневное'
+        ];
+
+        foreach ($questTypes as $questType) {
+            QuestType::create([
+                'name' => $questType
+            ]);
+        }
+    }
+}
