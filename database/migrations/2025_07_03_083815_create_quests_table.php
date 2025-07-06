75e23058d9b6ed7516bde2ecffea2aed4cd9c7b3
igorakimy <igorakimy@gmail.com>
2025-07-06 13:27:49 +0300

    add: quest model

---
+++ database/migrations/2025_07_03_083815_create_quests_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->tinyInteger('required_level')->default(1);
+            $table->unsignedInteger('gold')->default(0);
+            $table->unsignedInteger('experience')->default(0);
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->unsignedBigInteger('quest_type_id');
+            $table->unsignedBigInteger('quest_chain_id');
+            $table->unsignedBigInteger('npc_from_id');
+            $table->unsignedBigInteger('npc_to_id');
+            $table->timestamps();
+
+            $table->foreign('parent_id')
+                ->references('id')
+                ->on('quests')
+                ->onDelete('cascade');
+
+            $table->foreign('quest_type_id')
+                ->references('id')
+                ->on('quest_types')
+                ->onDelete('cascade');
+
+            $table->foreign('quest_chain_id')
+                ->references('id')
+                ->on('quest_chains')
+                ->onDelete('cascade');
+
+            $table->foreign('npc_from_id')
+                ->references('id')
+                ->on('npcs')
+                ->onDelete('cascade');
+
+            $table->foreign('npc_from_id')
+                ->references('id')
+                ->on('npcs')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quests');
+    }
+};
