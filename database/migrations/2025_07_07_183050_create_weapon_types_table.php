e7a9d970f17a07ab5b903ca2112679e41443f38c
igorakimy <igorakimy@gmail.com>
2025-07-07 21:47:20 +0300

    add: weapon type model

--- database/migrations/2025_07_07_183050_create_weapon_types_table.php
+++ database/migrations/2025_07_07_183050_create_weapon_types_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('equipment_types', function (Blueprint $table) {
+        Schema::create('weapon_types', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->string('plural_name');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('equipment_types');
+        Schema::dropIfExists('weapon_types');
     }
 };
